d3c91d0755b306a59813c9256c7352b00c13510e
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-10 20:16:09 +0530

    Update Streamlit_app.py
    new content

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale spinach and rocket smoothie')
+sreamlit.text('hard-boiled free-range egg')
