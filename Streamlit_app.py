c4ed688d0f0dc426177bb84abe261cb6f91dc658
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-10 20:23:22 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('my parents new healthy dinner')
+streamlit.title(':yum: my parents new healthy dinner')
 streamlit.header('breakfast menu')
 streamlit.text('omega 3 & blueberry oatmeal')
 streamlit.text('kale spinach and rocket smoothie')
 streamlit.text('hard-boiled free-range egg')
+streamlit.header('build your own fruit smoothie')
