3d7f8e01e489e9d768d1c491fc83523495cde881
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-15 03:14:04 +0530

    Update Streamlit_app.py
    quotes were missed in url fixed it

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -16,7 +16,7 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
 
 import requests
-fruityvice_response = requests.get(https://fruityvice.com/api/fruit/watermelon)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
