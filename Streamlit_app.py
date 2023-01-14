4e3c41d1caead592949fabf7d573c8c1a227acc8
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-15 02:58:36 +0530

    Update Streamlit_app.py
    snowfalke connection 1st query test 1uery

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -16,3 +16,15 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cur = my_cnx.cursor()
+
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from Snowflake:")
+
+streamlit.text(my_data_row)
