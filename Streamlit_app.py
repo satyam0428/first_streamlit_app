24115e34e8e5a4b9ee1da26643c9dcf74cb63dde
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-15 03:12:47 +0530

    Update Streamlit_app.py
    request library part that was missed all in one step

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -15,6 +15,21 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruit_to_show)
 
+import requests
+fruityvice_response = requests.get(https://fruityvice.com/api/fruit/watermelon)
+# streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
+#streamlit.text(fruityvice_response.json()) --removing this line as it is required to load text part into json
+
+# write your own comment -what does the next line do?
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+#below code kiwi is default value in case no value is supplied)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
