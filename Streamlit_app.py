fdeaaa4bc890d77fe7c0c5a4ac6dd2bde344ff55
satyam0428 <88152940+satyam0428@users.noreply.github.com>
2023-01-15 03:04:56 +0530

    Update Streamlit_app.py
    fetching all the rows now changing fetchone to fetchall

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,7 +23,7 @@ my_cur = my_cnx.cursor()
 
 my_cur.execute("SELECT * from fruit_load_list")
 
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 
 streamlit.header("the fruit load list contains:")
 
